7a7579e91b0f7b1de1e7c2ce70196dd0d9896743
daeun088 <kde2329@naver.com>
2023-07-07 15:27:21 +0900

    https://github.com/daeun088/summer-project.git

---
+++ src/test/java/summer/summerproject/SummerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package summer.summerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
